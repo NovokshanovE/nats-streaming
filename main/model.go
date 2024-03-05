7e743aec0ce90136211d134b0ae770a4b3fc861c
Evgeny Novokshanov <evgeny.novokshanov@gmail.com>
2024-03-06 00:51:26 +0300

    Добавлен функционал перевода из json в структуры(кэш)

--- main/model.go
+++ main/model.go
@@ -3,25 +3,58 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
-	"reflect"
 
 	_ "github.com/lib/pq"
 )
 
 type delivery struct {
-	Delivery_id string `json:"order_uid"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region"`
+	Email   string `json:"email"`
+}
+
+type payment struct {
+	Transaction   string `json:"transaction"`
+	Request_id    string `json:"request_id"`
+	Currency      string `json:"currency"`
+	Provider      string `json:"provider"`
+	Amount        int    `json:"amount"`
+	Payment_dt    int    `json:"payment_dt"`
+	Bank          string `json:"bank"`
+	Delivery_cost int    `json:"delivery_cost"`
+	Goods_total   int    `json:"goods_total"`
+	Custom_fee    int    `json:"custom_fee"`
+}
+
+type item struct {
+	Chrt_id      int    `json:"chrt_id"`
+	Track_number string `json:"track_number"`
+	Price        int    `json:"price"`
+	Rid          string `json:"rid"`
+	Name         string `json:"name"`
+	Sale         int    `json:"sale"`
+	Size         string `json:"size"`
+	Total_price  int    `json:"total_price"`
+	Nm_id        int    `json:"nm_id"`
+	Brand        string `json:"brand"`
+	Status       int    `json:"status"`
 }
 
 /*
 TODO: Необходимо допилить структуры
 */
 type Order struct {
-	Order_uid    string     `json:"order_uid"`
-	Track_number string     `json:"track_number"`
-	Entry        string     `json:"entry"`
-	Delivery     []delivery `json:"delivery"`
+	Order_uid    string   `json:"order_uid"`
+	Track_number string   `json:"track_number"`
+	Entry        string   `json:"entry"`
+	Delivery     delivery `json:"delivery"`
+	Payment      payment  `json:"payment"`
+	Items        []item   `json:"items"`
 }
 
 type Cache struct {
@@ -43,7 +76,7 @@ func (c Cache) from_json(json_str string) {
 	} // Десериализация JSON в структуру
 
 	c.Orders[len(c.Orders)] = order
-	log.Print(cache.Orders[len(cache.Orders)-1])
+	// log.Print(cache.Orders[len(cache.Orders)-1])
 
 }
 
@@ -94,11 +127,13 @@ func load_from_db() {
 			log.Fatal(err)
 		}
 
-		fmt.Print(reflect.TypeOf(data))
-		fmt.Print(data)
+		// fmt.Print(reflect.TypeOf(data))
+		// fmt.Print(data)
 		cache.from_db(id, data)
+
 		// cache[id] = data
 	}
+	log.Print(cache)
 }
 
 // req := Order{
